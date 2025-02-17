5ebf4705490c9782a43484bed6332e8031480ee0
ramhap1 <ramger020@hengersor.hu>
2025-02-17 11:06:30 +0100

    alaprendszer beállitása
    A bootstrap, React-bootstrap, router, tachyons telepítése
    Töröltük a szükségtelen állományokat
    Az összes css, main.jsx tartalma, csak a kötelező marad, és az app.jsx is csak ugyanezt tartalmazza.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1>Covid</h1>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
